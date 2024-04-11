28782e5764fc173b3bc4bf3217612dc8f380262e
Shandiya Ravichandran <shandiyaravigmail.com>
2024-04-11 13:51:14 +0200

    images rearranged

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import styles from './Hero.module.css';
-import heroimage from '../../assets/heroimage.png';
+import heroimage from '../../../assets/heroimage.png';
 
 const Hero = () => {
   return (
